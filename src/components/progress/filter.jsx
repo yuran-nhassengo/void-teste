928dbbb65632bfce270b3daa9173a0ae3db0496a
yuran-nhassengo <yurannhassengo@gmail.com>
2025-02-07 17:06:31 +0200

    updated Progress

--- src/components/progress/filter.jsx
+++ src/components/progress/filter.jsx
@@ -5,6 +5,7 @@ const Filter = ({ onFilterChange }) => {
   const [sector, setSector] = useState('');
   const [area, setArea] = useState('');
 
+
   const handlePesquisaChange = (e) => {
     setPesquisa(e.target.value);
     onFilterChange(e.target.value, sector, area); 
@@ -30,10 +31,19 @@ const Filter = ({ onFilterChange }) => {
         onChange={handlePesquisaChange}
       />
 
+    <div>
+        {/* Primeiro select - Sector */}
+        <select >
+          <option value="">Registo</option>
+          <option value="01">Registo 01</option>
+          <option value="02">Registo 02</option>
+        </select>
+      </div>
+
       <div>
         {/* Primeiro select - Sector */}
         <select value={sector} onChange={handleSectorChange}>
-          <option value="">Todos Sectores</option>
+          <option value="">Seleciona o Sector</option>
           <option value="Ribaue">Ribaue</option>
           <option value="Malema">Malema</option>
         </select>
@@ -42,7 +52,7 @@ const Filter = ({ onFilterChange }) => {
       <div>
         {/* Segundo select - Área */}
         <select value={area} onChange={handleAreaChange}>
-          <option value="">Todas Areas</option>
+          <option value="">Seleciona a area</option>
           <option value="Lalaue">Lalaue</option>
           <option value="Satique">Satique</option>
         </select>
